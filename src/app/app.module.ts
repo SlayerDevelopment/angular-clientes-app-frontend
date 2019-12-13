b43d8bcfd4b61fb802c9e2a7755823f0a51e8f34
Slayer Bryan <bryan.antonio.alvarado.villalobos@gmail.com>
2019-12-12 22:23:07 -0500

    Terminando aprender a usar visual studio code, conociendo el flujo de creacion de angular 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { DirectivaComponent } from './directiva/directiva.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    DirectivaComponent
   ],
   imports: [
     BrowserModule
