dfd466f80c75d7064dde5b8dfd5f5b7f91423f48
Slayer Bryan <bryan.antonio.alvarado.villalobos@gmail.com>
2019-12-13 20:49:44 -0500

    Terminando La seccion 3 del curso agregando dependecias de Bootstrap 4.1.3, Jquery 3.3.1 y popper.js 1.14.x para poder usar bootstrap sin la carpeta assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ClienteService } from './cliente/cliente.service';
 import { HeaderComponent } from './header/header.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -5,18 +6,28 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { DirectivaComponent } from './directiva/directiva.component';
+import { ClienteComponent } from './cliente/cliente.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const ROUTES: Routes = [
+  { path: '', redirectTo: '/cliente', pathMatch: 'full' },
+  { path: 'directivas', component: DirectivaComponent},
+  { path: 'cliente', component: ClienteComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    DirectivaComponent
+    DirectivaComponent,
+    ClienteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
   ],
-  providers: [],
+  providers: [ClienteService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/cliente/cliente.service.ts
@@ -0,0 +1,16 @@
+import { CLIENTES } from './cliente.json';
+import { Injectable } from '@angular/core';
+import { Cliente } from './cliente';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteService {
+
+  constructor() { }
+
+  getClientes(): Observable<Cliente[]> {
+    return of(CLIENTES);
+  }
+}
